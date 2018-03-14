113ec1e07831365d87ce5b64f2b8579530f6f31c
Matthew Roberts <web.matthewjr@gmail.com>
2018-03-14 11:19:12 +0000

    Initial Commit

---
+++ files/js/index.js
@@ -0,0 +1,39 @@
+
+let tabSelected = false;
+
+let decryptionForm = document.querySelector('#decryption');
+let encryptionForm = document.querySelector('#encryption');
+
+function SwitchTab() {
+    if(!tabSelected) {
+        decryptionForm.className = "active";
+        encryptionForm.className = "";
+    } else {
+        decryptionForm.className = "";
+        encryptionForm.className = "active";
+    }
+    tabSelected = !tabSelected;
+}
+
+function OnSubmitEncryption(e) {
+    e.preventDefault();
+    let data = {
+        technique: e.target[0].value,
+        input: e.target[1].value,
+        output: e.target[2].value
+    };
+    console.log(data);
+}
+
+function OnSubmitDecryption(e) {
+    e.preventDefault();
+    let data = {
+        technique: e.target[0].value,
+        input: e.target[1].value,
+        output: e.target[2].value
+    };
+    console.log(data);
+}
+
+encryptionForm.addEventListener('submit', OnSubmitEncryption);
+decryptionForm.addEventListener('submit', OnSubmitDecryption);
